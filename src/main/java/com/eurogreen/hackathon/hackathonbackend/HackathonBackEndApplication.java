948a50eacb782dfa463b16038e2d9f4a5c4b8b33
brianmcintyre <brianmcintyre@overstock.com>
2022-12-16 13:49:37 +0000

    Mock data

---
+++ src/main/java/com/eurogreen/hackathon/hackathonbackend/HackathonBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.eurogreen.hackathon.hackathonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurogreen/hackathon/hackathonbackend/HackathonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurogreen.hackathon.hackathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
